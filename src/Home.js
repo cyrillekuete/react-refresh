2b12910dabcd7de0fc4c758cc233dea6d525091e
Kuete Cyrille <cyrillekuete@gmail.com>
2023-01-19 03:54:30 +0100

    Handling Clicks Completed

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,23 @@
 const Home = () => {
-    return ( 
-        <div className="Home">
-            <h2>Home Page</h2>
-        </div>
-     );
-}
- 
+  const handleClick = (e) => {
+    console.log("Hello Ninjas");
+  };
+  const handleClickAgain = (name) => {
+    console.log("Hello" + name);
+  };
+  return (
+    <div className="Home">
+      <h2>Home Page</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button
+        onClick={(e) => {
+          handleClickAgain("Mario");
+        }}
+      >
+        Click Me Again
+      </button>
+    </div>
+  );
+};
+
 export default Home;
